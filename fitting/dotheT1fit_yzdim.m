fc657354e510a7aa697a0e8df64c25aa009a1261
Gustav Strijkers <g.j.strijkers@amsterdamumc.nl>
2019-09-10 13:52:23 +0200

    bug fixes

--- fitting/dotheT1fit_yzdim.m
+++ fitting/dotheT1fit_yzdim.m
@@ -6,45 +6,30 @@ function [T1out,M0out] = dotheT1fit_yzdim(input,mask,fa,tr)
 T1map = zeros(dimy,dimz,1);
 M0map = zeros(dimy,dimz,1);
 
-    function [F,J] = FJfun(x, xdata)
-        % Fitting function
-        F = x(1).*sin(pi*xdata/180).*(1 - exp(-tr./x(2)))./(1 - (cos(pi*xdata/180) * exp(-tr./x(2))));
-        if nargout > 1
-            % Jacobian
-            J = [ (sin((pi*xdata)/180).*(exp(-tr./x(2)) - 1))./(exp(-tr./x(2)).*cos((pi*xdata)/180) - 1), (tr.*x(1).*exp(-tr./x(2)).*sin((pi*xdata)/180))./(x(2).^2.*(exp(-tr./x(2)).*cos((pi*xdata)/180) - 1)) - (tr.*x(1).*exp(-tr./x(2)).*cos((pi*xdata)/180).*sin((pi*xdata)/180).*(exp(-tr./x(2)) - 1))./(x(2).^2.*(exp(-tr./x(2)).*cos((xdata.*pi)/180) - 1).^2)];
-        end
-    end
-
-    function H = Hfun(x, xdata)
-        % Fitting function
-        H = x(1).*sin(pi*xdata/180).*(1 - exp(-tr./x(2)))./(1 - (cos(pi*xdata/180) * exp(-tr./x(2))));
-        
-    end
-
-fitfun = @Hfun;        % or FJfun for fitting with Jacobian
+H = @(x,xdata)x(1).*sin(pi*xdata./180).*(1 - exp(-tr./x(2)))./(1 - (cos(pi.*xdata/180).*exp(-tr./x(2))));
 
 % starting value
-x0 = [max(input(:)) 500];
+x0 = double([max(input(:)) 1000]);
 
 % flip angles
-fa = [0,fa]';
+fa = double([0,fa]');
 
 % no display output, fitting with 'true' or without 'false' Jacobian
-opt = optimoptions('lsqcurvefit','SpecifyObjectiveGradient',false,'Diagnostics','off','Display','off','MaxIterations',50);
+opt = optimoptions('lsqcurvefit','Diagnostics','off','Display','off','MaxIterations',50);
 
 for j=1:dimy
     
     parfor k=1:dimz
         % for all z-coordinates
         
-        if mask(k) == 1
+        if mask(j,k) == 1
             % only fit when mask value indicates valid data point
             
             % pixel value as function of alpha
             ydata = double([0;squeeze(input(:,j,k))]);
             
             % do the fit
-            x = lsqcurvefit(fitfun,x0,fa,ydata,[0 0],[Inf 4000],opt);
+            x = lsqcurvefit(H,x0,fa,ydata,[0 0],[Inf 4000],opt);
             
             % make the maps
             T1map(j,k)=x(2);
