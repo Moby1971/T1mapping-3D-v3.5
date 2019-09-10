6065297c01de9d49ae48d8faf2226eb74b9a2413
Gustav Strijkers <g.j.strijkers@amsterdamumc.nl>
2019-09-10 13:09:35 +0200

    update

--- fitting/dotheT1fit_yzdim_despot.m
+++ fitting/dotheT1fit_yzdim_despot.m
@@ -6,36 +6,13 @@ function [T1out,M0out] = dotheT1fit_yzdim_despot(input,mask,fa,tr)
 T1map = zeros(dimy,dimz,1);
 M0map = zeros(dimy,dimz,1);
 
-%     function [F,J] = FJfun(x, xdata)
-%         % Fitting function
-%         F = x(1).*sin(pi*xdata/180).*(1 - exp(-tr./x(2)))./(1 - (cos(pi*xdata/180) * exp(-tr./x(2))));
-%         if nargout > 1
-%             % Jacobian
-%             J = [ (sin((pi*xdata)/180).*(exp(-tr./x(2)) - 1))./(exp(-tr./x(2)).*cos((pi*xdata)/180) - 1), (tr.*x(1).*exp(-tr./x(2)).*sin((pi*xdata)/180))./(x(2).^2.*(exp(-tr./x(2)).*cos((pi*xdata)/180) - 1)) - (tr.*x(1).*exp(-tr./x(2)).*cos((pi*xdata)/180).*sin((pi*xdata)/180).*(exp(-tr./x(2)) - 1))./(x(2).^2.*(exp(-tr./x(2)).*cos((xdata.*pi)/180) - 1).^2)];
-%         end
-%     end
-% 
-%     function H = Hfun(x, xdata)
-%         % Fitting function
-%         H = x(1).*sin(pi*xdata/180).*(1 - exp(-tr./x(2)))./(1 - (cos(pi*xdata/180) * exp(-tr./x(2))));
-%         
-%     end
-% 
-% fitfun = @Hfun;        % or FJfun for fitting with Jacobian
-
-% starting value
-x0 = [max(input(:)) 500];
-
 % flip angles
 fa = fa'*pi/180;
 lfa = length(fa);
 
-% no display output, fitting with 'true' or without 'false' Jacobian
-%opt = optimoptions('lsqcurvefit','SpecifyObjectiveGradient',false,'Diagnostics','off','Display','off','MaxIterations',50);
-
 
 for j=1:dimy
-    
+    % for all y-coordinates
     parfor k=1:dimz
         % for all z-coordinates
         
